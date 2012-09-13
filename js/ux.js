7d778e8d3af05600e20fc7342012bc198c8fc88b
João Magalhães <joamag@gmail.com>
2012-09-13 16:32:58 +0100

    new sort order validation

--- js/ux.js
+++ js/ux.js
@@ -8750,7 +8750,7 @@ jQuery.uxvisible = function(element, offset, delta, parent) {
             // the sorting list (tuple) to be used in the query
             var isAscending = sortSelected.hasClass("ascending");
             var sortOrder = isAscending ? "ascending" : "descending";
-            var sort = [sortValue, sortOrder];
+            var sort = sortValue ? [sortValue, sortOrder] : null;
 
             // retrieves the complete set of (graphical) filter lines to be
             // parser in search for the valid filters
