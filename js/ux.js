43e7b48e694e93a0b6c5866f323ae3a669220990
João Magalhães <joamag@hive.pt>
2012-10-04 20:48:00 +0100

    new binaries

--- js/ux.js
+++ js/ux.js
@@ -64,6 +64,7 @@
             var textArea = jQuery(".text-area", matchedObject).not(".template .text-area");
             var dropField = jQuery(".drop-field", matchedObject).not(".template .drop-field");
             var selectField = jQuery(".select-field", matchedObject).not(".template .select-field");
+            var checkField = jQuery(".check-field", matchedObject).not(".template .check-field");
             var radioField = jQuery(".radio-field", matchedObject).not(".template .radio-field");
             var tagField = jQuery(".tag-field", matchedObject).not(".template .tag-field");
             var incrementalField = jQuery(".incremental-field", matchedObject).not(".template .incremental-field");
@@ -145,6 +146,7 @@
             textArea.uxtextfield();
             dropField.uxdropfield();
             selectField.uxselectfield();
+            checkField.uxcheckfield();
             radioField.uxradiofield();
             tagField.uxtagfield();
             incrementalField.uxincrementalfield();
@@ -5617,6 +5619,108 @@ jQuery.uxvisible = function(element, offset, delta, parent) {
                         // may change their behavior in accordance
                         matchedObject.triggerHandler("change");
                     });
+        };
+
+        // initializes the plugin
+        initialize();
+
+        // returns the object
+        return this;
+    };
+})(jQuery);
+
+/**
+ * jQuery check field plugin, this jQuery plugin provides the base
+ * infra-structure for the creation of a check field component.
+ *
+ * @name jquery-check-field.js
+ * @author João Magalhães <joamag@hive.pt>
+ * @version 1.0
+ * @date March 10, 2010
+ * @category jQuery plugin
+ * @copyright Copyright (c) 2010-2012 Hive Solutions Lda.
+ * @license Hive Solutions Confidential Usage License (HSCUL) -
+ *          http://www.hive.pt/licenses/
+ */
+(function(jQuery) {
+    jQuery.fn.uxcheckfield = function(options) {
+        // the default values for the check field
+        var defaults = {};
+
+        // sets the default options value
+        var options = options ? options : {};
+
+        // constructs the options
+        var options = jQuery.extend(defaults, options);
+
+        // sets the jquery matched object
+        var matchedObject = this;
+
+        /**
+         * Initializer of the plugin, runs the necessary functions to initialize
+         * the structures.
+         */
+        var initialize = function() {
+            _appendHtml();
+            _registerHandlers();
+        };
+
+        /**
+         * Creates the necessary html for the component.
+         */
+        var _appendHtml = function() {
+            // iterates over all the matched object
+            matchedObject.each(function(index, element) {
+                        // retrieves the element refence
+                        var _element = jQuery(element);
+                        _update(_element, options);
+                    });
+        };
+
+        /**
+         * Registers the event handlers for the created objects.
+         */
+        var _registerHandlers = function() {
+            // retrieves the (possible) next check field label
+            // to register it for checking
+            var checkFieldLabel = matchedObject.next(".check-field-label");
+
+            // registers the check field label for the click
+            // event to check the associated check field
+            checkFieldLabel.click(function() {
+                // retrieves the current element and uses it to retrieve
+                // the previous check field
+                var element = jQuery(this);
+                 var checkField = element.prev(".check-field");
+
+                 // checks the "just" retrieved check field, this should
+                 // enable its boolean value
+                 _check(checkField, options);
+            });
+        };
+
+        var _check = function(matchedObject, options) {
+            // checks the current matched object by setting
+            // the its checked attribute
+            matchedObject.attr("checked", true);
+        };
+
+        var _update = function(matchedObject, options) {
+            // retrives the value and the checked value
+            var value = matchedObject.attr("value");
+            var checked = matchedObject.attr("data-checked");
+
+            // in case the checked value is the same
+            // as the value (current option)
+            if (checked == value) {
+                // sets the element as checked
+                matchedObject.attr("checked", true);
+            }
+            // otherwise it's not the selected element
+            else {
+                // sets the element as unchecked
+                matchedObject.attr("checked", false);
+            }
         };
 
         // initializes the plugin
@@ -13080,22 +13184,7 @@ jQuery.uxvisible = function(element, offset, delta, parent) {
             matchedObject.each(function(index, element) {
                         // retrieves the element refence
                         var _element = jQuery(element);
-
-                        // retrives the value and the checked value
-                        var value = _element.attr("value");
-                        var checked = _element.attr("data-checked");
-
-                        // in case the checked value is the same
-                        // as the value (current option)
-                        if (checked == value) {
-                            // sets the element as checked
-                            _element.attr("checked", true);
-                        }
-                        // otherwise it's not the selected element
-                        else {
-                            // sets the element as unchecked
-                            _element.attr("checked", false);
-                        }
+                        _update(_element, options);
                     });
         };
 
@@ -13103,6 +13192,46 @@ jQuery.uxvisible = function(element, offset, delta, parent) {
          * Registers the event handlers for the created objects.
          */
         var _registerHandlers = function() {
+            // retrieves the (possible) next radio field label
+            // to register it for checking
+            var radioFieldLabel = matchedObject.next(".radio-field-label");
+
+            // registers the radio field label for the click
+            // event to check the associated radio field
+            radioFieldLabel.click(function() {
+                        // retrieves the current element and uses it to retrieve
+                        // the previous radio field
+                        var element = jQuery(this);
+                        var radioField = element.prev(".radio-field");
+
+                        // checks the "just" retrieved radio field, this should
+                        // enable its boolean value
+                        _check(radioField, options);
+                    });
+        };
+
+        var _check = function(matchedObject, options) {
+            // checks the current matched object by setting
+            // the its checked attribute
+            matchedObject.attr("checked", true);
+        };
+
+        var _update = function(matchedObject, options) {
+            // retrives the value and the checked value
+            var value = matchedObject.attr("value");
+            var checked = matchedObject.attr("data-checked");
+
+            // in case the checked value is the same
+            // as the value (current option)
+            if (checked == value) {
+                // sets the element as checked
+                matchedObject.attr("checked", true);
+            }
+            // otherwise it's not the selected element
+            else {
+                // sets the element as unchecked
+                matchedObject.attr("checked", false);
+            }
         };
 
         // initializes the plugin
