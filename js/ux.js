9f1b9b07f078fd737cdfe58991b72428669068aa
joamag <joamag@gmail.com>
2012-10-21 19:57:56 +0100

    firefox tag field bug fixed

--- js/ux.js
+++ js/ux.js
@@ -17242,8 +17242,8 @@ jQuery.uxvisible = function(element, offset, delta, parent) {
             // and adds them together to calculate the extra pixel values
             // of the text field component
             var textFieldPaddingRight = parseInt(textField.css("padding-right"));
-            var textFieldBorderRight = parseInt(textField.css("border-right"));
-            var textFieldBorderLeft = parseInt(textField.css("border-left"));
+            var textFieldBorderRight = parseInt(textField.css("border-right-width"));
+            var textFieldBorderLeft = parseInt(textField.css("border-left-width"));
             var textFieldExtra = lineWidth + textFieldPaddingRight
                     + textFieldBorderRight + textFieldBorderLeft;
 
