6757cd498ce9f2d4707b06f6210596dc81f246e8
João Magalhães <joamag@hive.pt>
2013-01-13 18:22:33 +0000

    new float and integer plugins

--- js/ux.js
+++ js/ux.js
@@ -4069,6 +4069,42 @@ jQuery.uxvisible = function(element, offset, delta, parent) {
 })(jQuery);
 
 (function($) {
+    jQuery.fn.uxfloat = function() {
+        // sets the jquery matched object
+        var matchedObject = this;
+
+        // retrieves the string representation value
+        // from the matched object as uses it to convert
+        // it into a float value and verifies the conversion,
+        // in case it failed default to a zero value
+        var value = matchedObject.uxvalue();
+        var valueF = parseFloat(value);
+        valueF = isNaN(valueF) ? 0.0 : valueF;
+
+        // returns the value that was retrieved and converted
+        // into a float value
+        return valueF;
+    }
+})(jQuery);
+(function($) {
+    jQuery.fn.uxfint = function() {
+        // sets the jquery matched object
+        var matchedObject = this;
+
+        // retrieves the string representation value
+        // from the matched object as uses it to convert
+        // it into a integer value and verifies the conversion,
+        // in case it failed default to a zero value
+        var value = matchedObject.uxvalue();
+        var valueI = parseInt(value);
+        valueI = isNaN(valueI) ? 0 : valueF;
+
+        // returns the value that was retrieved and converted
+        // into a integer value
+        return valueI;
+    }
+})(jQuery);
+(function($) {
     jQuery.fn.uxprint = function(method, options) {
         // the default values for the enable
         var defaults = {};
