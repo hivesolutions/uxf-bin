2f1891d95fe20f813283c24b3046df01e7ae3ed0
João Magalhães <joamag@hive.pt>
2013-01-13 16:39:39 +0000

    new parent form refactor

--- js/ux.js
+++ js/ux.js
@@ -17781,7 +17781,9 @@ jQuery.uxvisible = function(element, offset, delta, parent) {
                 // retrieves the containing form
                 var parentForm = _element.parents("form");
 
-                // registers for the submit event
+                // registers for the submit event on the parent
+                // form, in order to be able to create the appropriate
+                // structures for proper submission
                 parentForm.submit(function() {
                             // retrieves the name of the element, this value is
                             // going to be used in the input element to be create
