edd3cdab3e1ebfaaa2e9b2912f9f1f17df470e76
joamag <joamag@gmail.com>
2013-01-22 19:58:18 +0000

    new round support

--- js/ux.js
+++ js/ux.js
@@ -440,6 +440,15 @@
 })(jQuery);
 
 (function($) {
+    jQuery.uxround = function(value, decimalPlaces) {
+        // rounds the specified value to the
+        // specified number of decimal places
+        var rounder = Math.pow(10, decimalPlaces);
+        return Math.round(value * rounder) / rounder;
+    };
+});
+
+(function($) {
     jQuery.fn.uxdataqueryjson = function(query, callback, options) {
         // the default timeout to be used in the request
         // (this value is used to delay the request)
