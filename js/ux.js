0977ae588ac2d84d27d4edf54a7fb561bf48b7bb
joamag <joamag@gmail.com>
2013-03-22 14:23:48 +0000

    support for removing trailing spaces

--- js/ux.js
+++ js/ux.js
@@ -12122,7 +12122,7 @@ jQuery.expr[":"].parents = function(a, i, m) {
                         // retrieves the complete set of (input) fields
                         // contained in the form  an itreates over them
                         // so that trailing spaces are removed
-                        var fields = jQuery(".text-field", element);
+                        var fields = jQuery(".text-field[data-object]", element);
                         !noTrim && fields.each(function(index, element) {
                                     var _element = jQuery(this)
                                     var value = _element.uxvalue();
