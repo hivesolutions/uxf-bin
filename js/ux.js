54bfa8132b8e1ebbbdb08064312ad4ee55dfa921
joamag <joamag@gmail.com>
2013-07-08 17:12:57 +0100

    better unregister support for key and shortcuts

--- js/ux.js
+++ js/ux.js
@@ -2866,39 +2866,50 @@ function onYouTubePlayerReady(id) {
                         // retrieves the target object base on the global option
                         var targetObject = global ? jQuery(document) : _element;
 
+                        // creates the handler function with a clojure in the current
+                        // enviroment that will change the location to the url defined
+                        // in the key element
+                        var handler = function(event) {
+                            // retrieves the key value
+                            var keyValue = event.keyCode
+                                    ? event.keyCode
+                                    : event.charCode
+                                            ? event.charCode
+                                            : event.which;
+
+                            // in case the ctrl or the alt keys
+                            // are pressed no need to handle the event
+                            // (default behavior is not desired)
+                            if (event.ctrlKey || event.altKey) {
+                                // returns immediately
+                                return;
+                            }
+
+                            // switches over the key value
+                            switch (keyValue) {
+                                // in case it's the target key
+                                case keyInteger :
+                                    // sets the "new" document location
+                                    jQuery.uxlocation(url);
+
+                                    // breaks the switch
+                                    break;
+
+                                // in case it's default
+                                default :
+                                    // breaks the switch
+                                    break;
+                            }
+                        };
+
                         // registers for the key up in the target
-                        // object element
-                        targetObject.keyup(function(event) {
-                                    // retrieves the key value
-                                    var keyValue = event.keyCode
-                                            ? event.keyCode
-                                            : event.charCode
-                                                    ? event.charCode
-                                                    : event.which;
-
-                                    // in case the ctrl or the alt keys
-                                    // are pressed no need to handle the event
-                                    // (default behavior is not desired)
-                                    if (event.ctrlKey || event.altKey) {
-                                        // returns immediately
-                                        return;
-                                    }
-
-                                    // switches over the key value
-                                    switch (keyValue) {
-                                        // in case it's the target key
-                                        case keyInteger :
-                                            // sets the "new" document location
-                                            jQuery.uxlocation(url);
-
-                                            // breaks the switch
-                                            break;
-
-                                        // in case it's default
-                                        default :
-                                            // breaks the switch
-                                            break;
-                                    }
+                        // object element and then sets the element
+                        // to unregister the handler once the element
+                        // is removed from the structures, avoiding extra
+                        // handling of events
+                        targetObject.keyup(handler);
+                        _element.bind("destroyed", function() {
+                                    targetObject.unbind("keyup", handler);
                                 });
                     });
         };
@@ -3264,41 +3275,51 @@ function onYouTubePlayerReady(id) {
                         // retrieves the target object base on the global option
                         var targetObject = global ? jQuery(document) : _element;
 
+                        // creates the handler function with a clojure in the current
+                        // enviroment that will increment and decrement the current page
+                        var handler = function(event) {
+                            // retrieves the key value
+                            var keyValue = event.keyCode
+                                    ? event.keyCode
+                                    : event.charCode
+                                            ? event.charCode
+                                            : event.which;
+
+                            // switches over the key value
+                            switch (keyValue) {
+                                // in case it's a j key
+                                case 74 :
+                                    // sends the current element to the
+                                    // next "element"
+                                    matchedObject.uxnext();
+
+                                    // breaks the switch
+                                    break;
+
+                                // in case it's a k key
+                                case 75 :
+                                    // sends the current element to the
+                                    // previous "element"
+                                    matchedObject.uxprevious();
+
+                                    // breaks the switch
+                                    break;
+
+                                // in case it's default
+                                default :
+                                    // breaks the switch
+                                    break;
+                            }
+                        }
+
                         // registers for the key up in the target
-                        // object element
-                        targetObject.keyup(function(event) {
-                                    // retrieves the key value
-                                    var keyValue = event.keyCode
-                                            ? event.keyCode
-                                            : event.charCode
-                                                    ? event.charCode
-                                                    : event.which;
-
-                                    // switches over the key value
-                                    switch (keyValue) {
-                                        // in case it's a j key
-                                        case 74 :
-                                            // sends the current element to the
-                                            // next "element"
-                                            matchedObject.uxnext();
-
-                                            // breaks the switch
-                                            break;
-
-                                        // in case it's a k key
-                                        case 75 :
-                                            // sends the current element to the
-                                            // previous "element"
-                                            matchedObject.uxprevious();
-
-                                            // breaks the switch
-                                            break;
-
-                                        // in case it's default
-                                        default :
-                                            // breaks the switch
-                                            break;
-                                    }
+                        // object element and then sets the element
+                        // to unregister the handler once the element
+                        // is removed from the structures, avoiding extra
+                        // handling of events
+                        targetObject.keyup(handler);
+                        _element.bind("destroyed", function() {
+                                    targetObject.unbind("keyup", handler);
                                 });
                     });
         };
