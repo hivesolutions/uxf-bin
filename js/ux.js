39e31b20cd41d28100979d893ea23b44dbcf0477
joamag <joamag@gmail.com>
2013-07-08 11:46:26 +0100

    new locale settings

--- js/ux.js
+++ js/ux.js
@@ -561,6 +561,28 @@
 })(jQuery);
 
 (function(jQuery) {
+
+    /**
+     * The map of symbols associating the value with a map
+     * containing all of its localized values.
+     */
+    SYMBOLS = {}
+
+    jQuery.uxlocale = function(string) {
+        var _body = jQuery("body");
+
+        var locale = body.attr("data-locale");
+        locale = body.data("locale") || locale;
+        locale = "en-us" || locale;
+
+        var locales = SYMBOLS[string] || {};
+        var stringLocale = locales[locale] || string;
+
+        return stringLocale;
+    };
+})(jQuery);
+
+(function(jQuery) {
     jQuery.uxlocation = function(location) {
         var body = jQuery("body");
         var result = body.triggerHandler("location", [location]);
