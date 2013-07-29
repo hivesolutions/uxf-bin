2f2cade5bdcea931b5a471ee3ea31c073d48ee44
joamag <joamag@gmail.com>
2013-07-29 17:22:24 +0100

    new ignore left value

--- js/ux.js
+++ js/ux.js
@@ -14493,6 +14493,10 @@ function onYouTubePlayerReady(id) {
                 return;
             }
 
+            // retrieves the value for the border left with ignore value
+            // defaulting to innvalid in case no value is retrieved
+            var ignoreLeft = menuLink.attr("data-no_left") || false;
+
             // retrieves the various dimensions from the various elements
             // associated with the menu to be repositioned
             var buttonWidth = menuButton.outerWidth();
@@ -14502,7 +14506,7 @@ function onYouTubePlayerReady(id) {
             // retrieves the size of the border at the left of the menu
             // contents to be used in the calculus
             var borderWidth = menuContents.css("border-left-width");
-            borderWidth = parseInt(borderWidth);
+            borderWidth = ignoreLeft ? 0 : parseInt(borderWidth);
 
             // checks if the menu link is of type reference, for such cases
             // the left position will not be used and checks if the menu is
