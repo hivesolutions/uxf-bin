b22e0446689db6811b1274ab1bbad9f2668c9207
joamag <joamag@gmail.com>
2013-08-27 11:19:12 +0100

    new reset event support

--- js/ux.js
+++ js/ux.js
@@ -5162,6 +5162,10 @@ function onYouTubePlayerReady(id) {
                     var object = _element.attr("data-object");
                     var method = _element["ux" + object]
                     method && method.call(_element, "reset");
+
+                    // triggers the reset event on the current element indicating
+                    // that a reset operation has been performed in it
+                    _element.triggerHandler("_reset");
                 });
     };
 })(jQuery);
