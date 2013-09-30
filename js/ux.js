ba5f90adaa50c4cc5bdcc28fa1e1463edbdc2c3f
joamag <joamag@gmail.com>
2013-09-30 20:28:40 +0100

    line support in canvas

--- js/ux.js
+++ js/ux.js
@@ -24905,6 +24905,13 @@ TemplateNode.prototype.getParameter = function(name) {
 var canvasRenderingContext = window.CanvasRenderingContext2D
         && CanvasRenderingContext2D.prototype || {};
 
+canvasRenderingContext.line = function(x1, y1, x2, y2) {
+    this.save();
+    this.moveTo(x1, y1);
+    this.lineTo(x2, y2);
+    this.restore();
+};
+
 canvasRenderingContext.dashedLine = function(x1, y1, x2, y2, parameters) {
     // in case the parameters are not defined the
     // default parameters are used, required
