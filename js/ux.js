345ae4652bed1c2ab0f7ec5c8f182b02530e994b
joamag <joamag@gmail.com>
2013-10-28 17:15:23 +0000

    new notification structure

--- js/ux.js
+++ js/ux.js
@@ -15581,7 +15581,7 @@ function onYouTubePlayerReady(id) {
         var defaults = {
             title : "Title",
             message : "Message",
-            timeout : 5000,
+            timeout : 7500,
             fadeTimeout : 500
         };
 
