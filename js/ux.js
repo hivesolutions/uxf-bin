b532b78c23c742dab90740e27b6bd573db1d8263
joamag <joamag@gmail.com>
2014-02-06 22:53:37 +0000

    new on enter avoid

--- js/ux.js
+++ js/ux.js
@@ -19296,6 +19296,15 @@ function onYouTubePlayerReady(id) {
 
                         // switches over the event key code
                         switch (eventKeyCode) {
+                            // in case it's the enter key
+                            case 13 :
+                                // stops the event propagation
+                                // (avoids extra problems in form)
+                                event.stopPropagation();
+                                event.preventDefault();
+
+                                // breaks the switch
+                                break;
 
                             // in case it's the page up key
                             case 33 :
