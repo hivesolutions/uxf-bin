5352f2fb17415736de7bd27a03313068a867319e
joamag <joamag@gmail.com>
2014-12-04 17:20:15 +0000

    initial ux support for animation

--- js/ux.js
+++ js/ux.js
@@ -146,6 +146,7 @@
             var video = jQuery(".video", matchedObject).not(".template .video");
             var code = jQuery(".code", matchedObject).not(".template .code");
             var uploader = jQuery(".uploader", matchedObject).not(".template .uploader");
+            var animation = jQuery(".animation", matchedObject).not(".template .animation");
             var transformFlip = jQuery(".transform-flip", matchedObject).not(".template .transform-flip");
             var fileDrop = jQuery(".file-drop", matchedObject).not(".template .file-drop");
             var imageUpload = jQuery(".image-upload", matchedObject).not(".template .image-upload");
@@ -252,6 +253,7 @@
             code.uxcode();
 
             // applies the various transform plugins
+            animation.uxanimation();
             transformFlip.uxtransformflip();
 
             // applies the various file plugins
@@ -6549,6 +6551,138 @@ function onYouTubePlayerReady(id) {
 })(jQuery);
 
 (function(jQuery) {
+    jQuery.fn.uxanimation = function(method, options) {
+        // the default values for the animation
+        var defaults = {};
+
+        // sets the default options value
+        var options = options ? options : {};
+
+        // constructs the options
+        var options = jQuery.extend(defaults, options);
+
+        // sets the jquery matched object
+        var matchedObject = this;
+
+        /**
+         * Initializer of the plugin, runs the necessary functions to initialize
+         * the structures.
+         */
+        var initialize = function() {
+            _appendHtml();
+            _registerHandlers();
+        };
+
+        /**
+         * Creates the necessary html for the component.
+         */
+        var _appendHtml = function() {
+            matchedObject.each(function(index, element) {
+                        var _element = jQuery(this);
+                        _register(_element, options);
+                    });
+        };
+
+        /**
+         * Registers the event handlers for the created objects.
+         */
+        var _registerHandlers = function() {
+        };
+
+        var _register = function(element, options) {
+            // retrieves the element values that are going to be used for
+            // some of the initial calculus for the animation
+            var height = element.height();
+            var duration = element.attr("data-duration") || "500";
+            duration = parseInt(duration);
+
+            // retrieves the background image reference and removes the
+            // proper url prefix from it, so that only the correct url
+            // is used for the processing of the image value
+            var imageUrl = element.css("background-image");
+            imageUrl = imageUrl.match(/^url\("?(.+?)"?\)$/);
+            imageUrl = imageUrl[1];
+
+            // creates a new image element and creates the structure based
+            // reference (element reference) for it
+            var image = new Image();
+            var imageElement = jQuery(image);
+
+            // registers the element for the load event so that the proper
+            // sprite height may be used in the initialization
+            imageElement.load(function() {
+                        var spriteHeight = image.height;
+                        _init(element, options, height, spriteHeight, duration);
+                    });
+
+            // updates the image url for the image element, this operation
+            // should trigger the loading of the image
+            image.src = imageUrl;
+        };
+
+        var _init = function(element, options, height, spriteHeight, duration) {
+            // "finalizes" any pending animation so that no two parallel
+            // animations will ever occur for the same element
+            _finalize(element, options);
+
+            // calculates the various partial values that are going
+            // to be used in the processing of the various frames from
+            // the "original" gathered values from the element
+            var frameCount = spriteHeight / height;
+            var frameTimeout = duration / frameCount;
+
+            // resets the various values of the element so that the
+            // animation is set to start from the beginning (orginal values)
+            element.data("height", height);
+            element.data("frame", 0);
+            element.data("count", frameCount);
+            element.data("timeout", frameCount);
+
+            // creates th interval that is going to be used for the animation
+            // with the frame timeout that has just been defined/calculated
+            var interval = window.setInterval(function() {
+                        _animate(element, options);
+                    }, frameTimeout);
+
+            // updates the reference to the interval in the element so that
+            // it may be refernced latter for cancellation
+            element.data("interval", interval);
+        };
+
+        var _finalize = function(element, options) {
+            // retrieves any registered interval for the element and then
+            // cancels/clears it so that no more animation occurs
+            var interval = element.data("interval");
+            interval && clearInterval(interval);
+        };
+
+        var _animate = function(element, options) {
+            // retrieves the current state value of the element to be used
+            // in the calculus of the current animation iteration
+            var height = element.data("height");
+            var index = element.data("frame");
+            var frameCount = element.data("count");
+
+            // calculates the "new" or next index value taking the previous
+            // one as reference and the re-calculates the offset value
+            var next = index == frameCount - 1 ? 0 : index + 1;
+            var offset = height * next * -1;
+
+            // updates the element's css position and the frame (index) value
+            // of the current animation, to be used in next iteration
+            element.css("background-position-y", offset + "px");
+            element.data("frame", next);
+        };
+
+        // initializes the plugin
+        initialize();
+
+        // returns the object
+        return this;
+    };
+})(jQuery);
+
+(function(jQuery) {
     jQuery.fn.uxalert = function(message, callback, options) {
         // the default values for the alert
         var defaults = {};
@@ -8650,7 +8784,7 @@ function onYouTubePlayerReady(id) {
  */
 (function(jQuery) {
     jQuery.fn.uxcrosslist = function(method, options) {
-        // the default values for the panel
+        // the default values for the plugin
         var defaults = {};
 
         // sets the default method value
@@ -17167,7 +17301,7 @@ function onYouTubePlayerReady(id) {
  */
 (function(jQuery) {
     jQuery.fn.uxpanelmore = function(options) {
-        // the default values for the panel
+        // the default values for the plugin
         var defaults = {};
 
         // sets the default options value
@@ -17280,7 +17414,7 @@ function onYouTubePlayerReady(id) {
  */
 (function(jQuery) {
     jQuery.fn.uxpanelstack = function(method, options) {
-        // the default values for the panel
+        // the default values for the plugin
         var defaults = {};
 
         // sets the default method value
@@ -17454,7 +17588,7 @@ function onYouTubePlayerReady(id) {
  */
 (function(jQuery) {
     jQuery.fn.uxpanel = function(method, options) {
-        // the default values for the panel
+        // the default values for the plugin
         var defaults = {};
 
         // sets the default method value
@@ -18561,7 +18695,7 @@ function onYouTubePlayerReady(id) {
  */
 (function(jQuery) {
     jQuery.fn.uxselectlist = function(method, options) {
-        // the default values for the panel
+        // the default values for the plugin
         var defaults = {};
 
         // sets the default method value
@@ -19226,7 +19360,7 @@ function onYouTubePlayerReady(id) {
  */
 (function(jQuery) {
     jQuery.fn.uxsliderbar = function(method, options) {
-        // the default values for the panel
+        // the default values for the plugin
         var defaults = {};
 
         // sets the default method value
@@ -20117,7 +20251,7 @@ function onYouTubePlayerReady(id) {
  */
 (function(jQuery) {
     jQuery.fn.uxsourcelist = function(method, options) {
-        // the default values for the panel
+        // the default values for the plugin
         var defaults = {};
 
         // sets the default method value
@@ -21716,7 +21850,7 @@ function onYouTubePlayerReady(id) {
  */
 (function(jQuery) {
     jQuery.fn.uxtagfield = function(method, options) {
-        // the default values for the panel
+        // the default values for the plugin
         var defaults = {};
 
         // sets the default method value
