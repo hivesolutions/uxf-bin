b4648f30d1d513952dfaaa06c792b7a3e72680d7
joamag <joamag@gmail.com>
2015-03-09 12:39:09 +0000

    new drop tag support

--- js/ux.js
+++ js/ux.js
@@ -17695,7 +17695,7 @@ function onYouTubePlayerReady(id) {
             // registers the resize in the window, this is a custom
             // event triggered at the correct timing of the window
             // sizing workflow, this will trigger a delayed based
-            // resizing opertion on the oeverlay element
+            // resizing operation on the oeverlay element
             _window.bind("size", function(event) {
                         // resizes the overlay in the screen
                         resizeDelay(matchedObject, options);
