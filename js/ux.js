7ed19adfe45476de427b911c5b7837f661ffdfb2
joamag <joamag@gmail.com>
2015-04-10 22:33:00 +0100

    new ilegal verification

--- js/ux.js
+++ js/ux.js
@@ -5342,8 +5342,8 @@ function onYouTubePlayerReady(id) {
                     // note that the attribute value is first verified for
                     // possible illegal values and skipped if it's illegal
                     var attribute = _element.attr(attrName);
-                    var isLegal = attribute.match(ATTR_REGEX) == null;
-                    if (isLegal) {
+                    var isIlegal = attribute.match(ATTR_REGEX) != null;
+                    if (isIlegal) {
                         return;
                     }
                     _element.attr(attrNameTarget, attribute);
