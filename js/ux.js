653d259545bea035a88be39ce0f541592dd47725
joamag <joamag@hive.pt>
2016-05-17 10:28:29 +0100

    small code fix

--- js/ux.js
+++ js/ux.js
@@ -17978,7 +17978,7 @@ function onYouTubePlayerReady(id) {
             // the localization process for both the title and the message,
             // this is considered the default behaviour for info
             title = title || "Information";
-            fullMonth = jQuery.uxlocale(title);
+            title = jQuery.uxlocale(title);
             message = jQuery.uxlocale(message);
 
             // sets the window properties so that it displays the proper values
