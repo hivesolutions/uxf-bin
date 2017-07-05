433daa03d99056bd864701791ad089118957f950
joamag <joamag@gmail.com>
2017-07-05 18:31:22 +0100

    new beautify

--- js/ux.js
+++ js/ux.js
@@ -25204,7 +25204,9 @@ function onYouTubePlayerReady(id) {
                 // in the processing of the paste event on the table
                 var column = element.parents("td");
                 var table = column.parents(".table");
-                var target = jQuery("> [data-object]:not([disabled], .disabled)", column);
+                var target = jQuery(
+                    "> [data-object]:not([disabled], .disabled)",
+                    column);
 
                 // verifies if the paste feature has been explicitly disabled for
                 // the current table and if that's the case returns immedidately
@@ -25229,11 +25231,13 @@ function onYouTubePlayerReady(id) {
                 // normalizes the text data, so that no extra (not required) operations
                 // are going to be performed from the content of it (optimization)
                 textData = textData.strip("\n");
+                textData = textData.strip("\r");
                 textData = textData.strip("\t");
 
                 // verifies if the provided text data is valid for the structured paste
                 // oepration and if that's not the case reuturns immedidately
-                var isValid = textData.indexOf("\n") != -1 || textData.indexOf("\t") != -1;
+                var isValid = textData.indexOf("\n") != -1 || textData.indexOf("\r") != -1 ||
+                    textData.indexOf("\t") != -1;
                 if (!isValid) {
                     return;
                 }
@@ -25264,8 +25268,10 @@ function onYouTubePlayerReady(id) {
                     for (var _index = 0; _index < columns.length; _index++) {
                         // tries to retrieve the current element to be used in the
                         // operation that is going to populate the value
-                        current = initial ? initial : _next(current,
-                            "> [data-object]:not([disabled], .disabled)", null, null, true);
+                        current = initial ? initial : _next(
+                            current,
+                            "> [data-object]:not([disabled], .disabled)",
+                            null, null, true);
                         initial = null;
                         if (!current) {
                             break;
@@ -25290,7 +25296,9 @@ function onYouTubePlayerReady(id) {
                     // in iteration and then uses it to retrieve the last column
                     var row = current.parents("tr");
                     var lastColumn = jQuery("td.last", row);
-                    initial = _next(null, "> [data-object]:not([disabled], .disabled)",
+                    initial = _next(
+                        null,
+                        "> [data-object]:not([disabled], .disabled)",
                         lastColumn, null, true);
 
                     // in case the next initial is an invalid one (not possible to set it)
