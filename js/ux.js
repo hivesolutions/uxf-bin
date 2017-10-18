8701dd52ac53c331597fe305b05aadd4fa43c5a7
joamag <joamag@gmail.com>
2017-10-18 20:23:05 +0100

    new separators

--- js/ux.js
+++ js/ux.js
@@ -30448,7 +30448,10 @@ Number.SYMBOLS = {
     "KRW": ["₩", -1],
     "CHF": ["fr.", 1],
     "SGD": ["$", -1],
-    "MXN": ["$", -1]
+    "MXN": ["$", -1],
+    "DKK": ["kr.", 1],
+    "SEK": ["kr.", 1],
+    "PLN": ["zl", 1]
 };
 
 Number.DECIMAL_PLACES = {
@@ -30463,7 +30466,10 @@ Number.DECIMAL_PLACES = {
     "KRW": 0,
     "CHF": 2,
     "SGD": 2,
-    "MXN": 2
+    "MXN": 2,
+    "DKK": 2,
+    "SEK": 2,
+    "PLN": 2
 };
 
 Number.SEPARATOR = {
@@ -30478,7 +30484,10 @@ Number.SEPARATOR = {
     "KRW": ".",
     "CHF": ".",
     "SGD": ".",
-    "MXN": "."
+    "MXN": ".",
+    "DKK": ".",
+    "SEK": ",",
+    "PLN": "."
 };
 
 Number.THOUSANDS = {
@@ -30493,7 +30502,10 @@ Number.THOUSANDS = {
     "KRW": ",",
     "CHF": ",",
     "SGD": ",",
-    "MXN": ","
+    "MXN": ",",
+    "DKK": ",",
+    "SEK": ".",
+    "PLN": " "
 };
 
 Number.prototype.formatMoney = function(places, separator, thousands, currency, useSymbol) {
